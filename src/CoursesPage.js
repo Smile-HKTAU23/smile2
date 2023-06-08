b090db187b97ddb2422bc9ef903bfc08f453d4c9
barak <barakkol@gmail.com>
2023-06-08 23:38:06 +0300

    working, no 2 load

--- src/CoursesPage.js
+++ src/CoursesPage.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import CardSwiper from './Maps.js';
 
 const CoursesPage = () => {
   const location = useLocation();
   console.log(location.state)
+  let cards = location.state.map((loc) => {
+    return {
+      id:loc.id,
+      lat:loc.pickup.lat,
+      lng:loc.pickup.lng,
+    }
+  }
+  );  
+
 
   return (
     <div>
       <h2>Results Page</h2>
       <p>Destination: </p>
+      <CardSwiper cards={cards} locations={location.state}/>
     </div>
   );
 };
