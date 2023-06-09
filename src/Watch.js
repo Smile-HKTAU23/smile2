3b87c8df244fe288ab71bf1e1772b7653c38a15e
barak <barakkol@gmail.com>
2023-06-09 05:46:54 +0300

    updating

--- src/Watch.js
+++ src/Watch.js
@@ -1,131 +1,108 @@
-import React, { useEffect, useState,  useRef } from 'react';
-import './Maps.css'
-import { DirectionsRenderer, Marker, GoogleMap, DirectionsService } from '@react-google-maps/api';
+import React, { useEffect, useState, useRef } from 'react';
+import './Maps.css';
+import {
+  DirectionsRenderer,
+  Marker,
+  GoogleMap,
+  DirectionsService,
+} from '@react-google-maps/api';
 import io from 'socket.io-client';
 import { useLocation } from 'react-router-dom';
 
 const Watch = () => {
+  const [directions, setDirections] = useState(null);
+  const [pos, setPos] = useState({ lat: 32.069235, lng: 34.825947 });
+  const [pos2, setPos2] = useState({ lat: 32.069235, lng: 34.825947 });
+  const [driverPosition, setDriverPosition] = useState({
+    lat: 32.110820,
+    lng: 34.806710,
+  });
+  const [currentMap, setCurrentMap] = useState(null);
 
-    const [directions, setDirections] = useState(null);
+  const parameters_location = useLocation();
+  const card = parameters_location.state.card;
 
-    const parameters_location = useLocation();
-    const card = parameters_location.state.card
-    let pos = { lat: 32.069235, lng: 34.825947 };
-    let pos2 = { lat: 32.069235, lng: 34.825947 };
-    let driverPosition = {lat: 32.110820, lng:34.806710}
-    
-    let currentMap = null;
-    const containerStyle = {
-      width: '100%',
-      height: '51rem'
-    };
-
-  const setDriversPosition = (newPos) => {
-    driverPosition = newPos;
-  }
-
-  const setPos = (newPos) => {
-    pos = newPos;
-  }
-
-  const setPos2 = (newPos) => {
-    pos2 = newPos;
-  }
+  const containerStyle = {
+    width: '100%',
+    height: '51rem',
+  };
 
   const options = {
-    disableDefaultUI: true
-  }
+    disableDefaultUI: true,
+  };
 
   const handleDirectionsService = (directionsService) => {
-    if (directions == null)
-      {
-        console.log(directionsService)
-        setDirections(directionsService)  
-      }            
+    if (directions == null) {
+      setDirections(directionsService);
+    }
   };
-  
+
   const getCurrentMarkers = () => {
-    const markerData = [
-      pos,
-      driverPosition,
-      pos2
-      // Add more marker data as needed
-    ];
-  
-    // Create an array of <Marker> components based on the marker data
+    const markerData = [pos, driverPosition, pos2];
+
     const markers = markerData.map((marker, index) => (
       <Marker
-        key={`marker_${index}`} // Unique key for each marker
+        key={`marker_${index}`}
         position={{
           lat: marker.lat,
           lng: marker.lng,
         }}
       />
     ));
-  
+
     return markers;
-  
-  }
+  };
 
   const renderMap = (card) => {
-    return (<GoogleMap
+    return (
+      <GoogleMap
         mapContainerStyle={containerStyle}
-        center={driverPosition}
+        center={card.pickup}
         zoom={8}
         options={options}
       >
-        
-      {directions && <DirectionsRenderer directions={directions} />}
+        {directions && <DirectionsRenderer directions={directions} />}
 
-      <DirectionsService options={{
-          destination: card.dropoff,
-          origin: card.pickup,
-          travelMode: 'DRIVING',
-        }} callback={handleDirectionsService} />
+        <DirectionsService
+          options={{
+            destination: card.dropoff,
+            origin: card.pickup,
+            travelMode: 'DRIVING',
+          }}
+          callback={handleDirectionsService}
+        />
 
-      {getCurrentMarkers()}
-        
-      </GoogleMap>)
-  }
-
-  const setCurrentMap = (newMap) => {
-    currentMap = newMap;
-  }
-  setCurrentMap(renderMap(card));
+        {getCurrentMarkers()}
+      </GoogleMap>
+    );
+  };
 
   const socketRef = useRef(null);
 
   useEffect(() => {
-    // Establish the WebSocket connection
     socketRef.current = io('http://localhost:5000');
 
-    // Send the "hello" message every minute
     const interval = setInterval(() => {
-      socketRef.current.emit('message', {'hello':'1'});
+      socketRef.current.emit('message', { hello: '1' });
     }, 1000);
 
-    socketRef.current.on('response', (data) => {      
-        console.log(data.driver_location)
-        console.log(data.passenger_location)
-        setDriversPosition(data.driver_location)
-        setPos(data.passenger_location);
-        setPos2(data.passenger_destination);       
-        setCurrentMap(renderMap(card))              
-        
-      });
+    socketRef.current.on('response', (data) => {
+      setDriverPosition(data.driver_location);
+      setPos(data.passenger_location);
+      setPos2(data.passenger_destination);
+    });
 
-    // Clean up the interval and close the WebSocket connection on component unmount
     return () => {
       clearInterval(interval);
       socketRef.current.close();
     };
   }, []);
 
-    return (
-      <div className="card-swiper">
-        {currentMap}
-      </div>
-    );
-  };
+  useEffect(() => {
+    setCurrentMap(renderMap(card));
+  }, [driverPosition, pos, pos2]);
 
-  export default Watch;
+  return <div className="card-swiper">{currentMap}</div>;
+};
+
+export default Watch;
